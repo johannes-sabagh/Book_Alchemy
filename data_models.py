e9e27229622d816533e3cbfa7c2b5d05ce2ecfa4
johannes-sabagh <johannes.sabagh@gmail.com>
2026-03-22 13:09:07 +0100

    created core structure and the tables

---
+++ data_models.py
@@ -0,0 +1,21 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String
+
+db = SQLAlchemy()
+
+
+class Author(db.Model):
+    __tablename__ = "authors"
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String)
+    birth_date = Column(String)
+    date_of_death = Column(String)
+
+class Book(db.Model):
+    __tablename__ = "books"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    isbn = Column(String)
+    title = Column(String)
+    publication_year = Column(String)
+    author_id = Column(Integer)
+
