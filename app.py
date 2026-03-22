98a6031e2f4eaa9debd445791da110f63243c6c7
johannes-sabagh <johannes.sabagh@gmail.com>
2026-03-22 14:18:34 +0100

    added add_author and it works

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, render_template, session
 from flask_sqlalchemy import SQLAlchemy
 import os
 from data_models import db, Author, Book
+
 
 app = Flask(__name__)
 
@@ -11,5 +12,39 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 db.init_app(app)
 
 
-with app.app_context():
-  db.create_all()
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+  if request.method == 'GET':
+    return render_template('add_author.html')
+  add_name = request.form.get('name')
+  add_birth_date = request.form.get('birthdate')
+  add_date_of_death = request.form.get('date_of_death')
+
+  author = Author(
+    name = add_name,
+    birth_date = add_birth_date,
+    date_of_death = add_date_of_death
+  )
+  db.session.add(author)
+  db.session.commit()
+  return render_template('add_author.html')
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
+
+
+
+
+
+
+
+
+
+#with app.app_context():
+  #db.create_all()
