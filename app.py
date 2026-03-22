32aec61f1894bb51a593c62381eaac2b111a1b73
johannes-sabagh <johannes.sabagh@gmail.com>
2026-03-22 16:27:43 +0100

    added add_book and it's working

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ db.init_app(app)
 def add_author():
   if request.method == 'GET':
     return render_template('add_author.html')
-  add_name = request.form.get('name')
-  add_birth_date = request.form.get('birthdate')
-  add_date_of_death = request.form.get('date_of_death')
+  add_name = request.form.get('name', '')
+  add_birth_date = request.form.get('birthdate', '')
+  add_date_of_death = request.form.get('date_of_death', '')
 
   author = Author(
     name = add_name,
@@ -27,11 +27,31 @@ def add_author():
   )
   db.session.add(author)
   db.session.commit()
-  return render_template('add_author.html')
+  msg = f'The author {add_name} has been added successfully!'
+  return render_template('add_author.html', msg=msg)
 
 
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+  authors = db.session.query(Author) \
+    .all()
+  if request.method == 'GET':
+    return render_template('add_book.html', authors=authors)
+  add_isbn = request.form.get('isbn')
+  add_title = request.form.get('title')
+  add_publication_year = request.form.get('publication_year')
+  add_author_id = request.form.get('authors')
 
-
+  book = Book(
+    isbn = add_isbn,
+    title = add_title,
+    publication_year = add_publication_year,
+    author_id = add_author_id
+  )
+  db.session.add(book)
+  db.session.commit()
+  msg = f"The book {add_title} has been added successfully"
+  return render_template('add_book.html', authors=authors, msg=msg)
 
 
 
